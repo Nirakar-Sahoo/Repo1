af0999d445465949659f30c385906ba17f75a2e3
Nirakar Sahoo <sahoonirakar1111@gmail.com>
2022-03-11 22:52:10 +0530

    m2 method addedin utility branch1 by dev1

--- DemoProj1/src/com/nt/util/Utility.java
+++ DemoProj1/src/com/nt/util/Utility.java
@@ -11,6 +11,9 @@ public class Utility {
 	public void m1() {
 		System.out.println("Utility.m1()....123456");
 		System.out.println("hello");
-
+	}
+	
+	public void m2() {
+		System.out.println("Utility.m2()");
 	}
 }
