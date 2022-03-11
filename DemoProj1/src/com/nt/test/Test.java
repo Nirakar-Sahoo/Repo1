23102e572cc08240f4cf2a8cd8a2845bf900ce05
Nirakar Sahoo <sahoonirakar1111@gmail.com>
2022-03-11 21:09:37 +0530

    test.java modified by Dev1

--- DemoProj1/src/com/nt/test/Test.java
+++ DemoProj1/src/com/nt/test/Test.java
@@ -1,7 +1,10 @@
 package com.nt.test;
 
+import com.nt.util.Utility;
+
 public class Test {
 	public static void main(String[] args) {
-		
+		Utility utility=new Utility();
+		System.out.println(utility.showDate());
 	}
 }
